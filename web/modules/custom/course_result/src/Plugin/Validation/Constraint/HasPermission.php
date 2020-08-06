c4f0a603047298ea12095eaf87dbbb85759cd489
Usai <kaurusai@gmail.com>
2020-08-06 14:42:18 +0300

    Has Permission files

---
+++ web/modules/custom/course_result/src/Plugin/Validation/Constraint/HasPermission.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\course_result\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Constraint(
+ *   id = "HasPermission",
+ *   label = @Translation("User has permission, context = "Validation"),
+ *   type = "string"
+ */
+/*class HasPermission extends Constraint {
+
+  // The message that will be shown if the value does not have permission.
+  public $hasNoPermission = 'User  does not have required permission %permission';
+
+}*/
---
+++ web/modules/custom/course_result/src/Plugin/Validation/Constraint/HasPermissionValidator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\course_result\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+
+/*class HasPermissionValidator extends ConstraintValidator {
+
+
+  public function validate($value, Constraint $constraint) {
+  }
+}*/
