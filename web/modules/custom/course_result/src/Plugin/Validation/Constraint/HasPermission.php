1a8d15dbd54ea78d1954b815908bbc9163cd2115
Usai <kaurusai@gmail.com>
2020-08-12 13:34:48 +0300

    Permission validator

--- web/modules/custom/course_result/src/Plugin/Validation/Constraint/HasPermission.php
+++ web/modules/custom/course_result/src/Plugin/Validation/Constraint/HasPermission.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\course_result\Plugin\Validation\Constraint;
 
+use Drupal\Core\Entity\Plugin\Validation\Constraint\EntityTypeConstraint;
 use Symfony\Component\Validator\Constraint;
 
 /**
@@ -10,13 +11,13 @@ use Symfony\Component\Validator\Constraint;
  * @Constraint(
  *   id = "HasPermission",
  *   label = @Translation("Just work"),
- *   type = {"entity"}
+ *   type = { "integer", "string" }
  * )
  */
 class HasPermission extends Constraint {
 
   // The message that will be shown if the value does not have permission.
-  public $hasNoPermission = 'User  does not have required permission %permission';
+  public $hasNoPermission = 'User  does not have required permission';
 
   public $needsValue = '%field-name field cannot be empty at the time of publication.';
 
--- web/modules/custom/course_result/src/Plugin/Validation/Constraint/HasPermissionValidator.php
+++ web/modules/custom/course_result/src/Plugin/Validation/Constraint/HasPermissionValidator.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\course_result\Plugin\Validation\Constraint;
 
+use Drupal\Core\Entity\Plugin\Validation\Constraint\EntityTypeConstraintValidator;
+use Drupal\user\Entity\User;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
@@ -10,11 +12,15 @@ class HasPermissionValidator extends ConstraintValidator {
 
 
   public function validate($value, Constraint $constraint) {
+      $this->context->addViolation($constraint->hasNoPermission);
 
-    $a = 1;
-
-    if(true){
-
-    }
+/*    if(!empty($value->getEntity()->course_participant_uid->target_id)){
+      $userId = $value->getEntity()->course_participant_uid->target_id;
+      $user = User::load($userId);
+      if($user && !$user->hasPermission('participate in course')){
+        $this->context->addViolation($constraint->hasNoPermission);
+        $b = 2;
+      }
+    }*/
   }
 }
