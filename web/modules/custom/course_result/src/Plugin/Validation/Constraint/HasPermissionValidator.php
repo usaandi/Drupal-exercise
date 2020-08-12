084adbde81b6869076ce88b72c1509b1207f87fb
Usai <kaurusai@gmail.com>
2020-08-12 15:21:36 +0300

    Fixed PermissionValidator

--- web/modules/custom/course_result/src/Plugin/Validation/Constraint/HasPermissionValidator.php
+++ web/modules/custom/course_result/src/Plugin/Validation/Constraint/HasPermissionValidator.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\course_result\Plugin\Validation\Constraint;
 
-use Drupal\Core\Entity\Plugin\Validation\Constraint\EntityTypeConstraintValidator;
 use Drupal\user\Entity\User;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
@@ -11,16 +10,17 @@ use Symfony\Component\Validator\ConstraintValidator;
 class HasPermissionValidator extends ConstraintValidator {
 
 
+  /**
+   * @param mixed $value
+   * @param Constraint $constraint
+   */
   public function validate($value, Constraint $constraint) {
-      $this->context->addViolation($constraint->hasNoPermission);
-
-/*    if(!empty($value->getEntity()->course_participant_uid->target_id)){
+    if (!empty($value->getEntity()->course_participant_uid->target_id)) {
       $userId = $value->getEntity()->course_participant_uid->target_id;
       $user = User::load($userId);
-      if($user && !$user->hasPermission('participate in course')){
+      if ($user && !$user->hasPermission('participate in course')) {
         $this->context->addViolation($constraint->hasNoPermission);
-        $b = 2;
       }
-    }*/
+    }
   }
 }
